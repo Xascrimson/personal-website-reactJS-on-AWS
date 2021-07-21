0e4faf7bbb07a87d39dd48237433c0e2313f08ff
Xascrimson <35974973+Xascrimson@users.noreply.github.com>
2021-07-21 22:11:47 +1000

    first commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Nav } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Blog from "./Blog";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AppBar, Toolbar,IconButton,Typography } from "@material-ui/core";
+import {MenuIcon} from "@material-ui/icons";
+
+export default function App() {
+	return (
+		<Router>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+					>
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						News
+					</Typography>
+					<Button color="inherit">Login</Button>
+				</Toolbar>
+			</AppBar>
+
+			{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+			<Switch>
+				<Route path="/about">
+					<About />
+				</Route>
+				<Route path="/users">
+					<Users />
+				</Route>
+
+				<Route path="/Danny">
+					<Danny />
+				</Route>
+
+				<Route path="/">
+					<Blog />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App;
+function About() {
+	return <h2>About</h2>;
+}
+
+function Users() {
+	return <h2>Users</h2>;
+}
+
+function Danny() {
+	return <h2> hello danny</h2>;
+}
---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Markdown from './Markdown';
+
+const useStyles = makeStyles((theme) => ({
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0),
+  },
+}));
+
+export default function Home(props) {
+  const classes = useStyles();
+  const { posts, title } = props;
+
+  return (
+    <p>hello</p>
+  );
+}
+
+Home.propTypes = {
+  posts: PropTypes.array,
+  title: PropTypes.string,
+};
