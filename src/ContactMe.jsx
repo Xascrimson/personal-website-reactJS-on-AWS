d3db7056a89a6c3ba9eba6313feedcde1b3ca3c1
danny huang <dannyhuang1234@hotmail.com>
2021-09-26 23:52:01 +1300

    adding address

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogTitle, List, ListItem } from "@material-ui/core";
-import { Email, Phone } from "@material-ui/icons";
+import { Drafts, Home, Phone } from "@material-ui/icons";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import React, { useState } from "react";
@@ -8,14 +8,22 @@ export function ContactInfo({ open, onClose }) {
 	return (
 		<Dialog onClose={onClose} open={open}>
 			<DialogTitle>Contact Details</DialogTitle>
-			<List sx={{ pt: 20 }}>
+			<List>
+				<ListItem>
+					<ListItem>
+						<ListItemIcon>
+							<Home />
+						</ListItemIcon>
+						Southbank, Melbourne, VIC, 3006, Australia
+					</ListItem>
+				</ListItem>
 				<ListItem>
 					<ListItemButton
 						component="a"
 						href="mailto:dannyhuang1234@gmail.com"
 					>
 						<ListItemIcon>
-							<Email />
+							<Drafts />
 						</ListItemIcon>
 						dannyhuang1234@gmail.com
 					</ListItemButton>
